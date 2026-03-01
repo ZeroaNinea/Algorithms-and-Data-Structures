42727a759dbdba8bc86634f2a704f10eec209cda
Catgirl <zeroaninea@gmail.com>
2026-03-01 22:10:55 +0400

    log nums added

--- index.ts
+++ index.ts
@@ -1,7 +1,12 @@
 import { toFahrenheit, toCelsius } from './celsius-fahrenheit/index.js';
+import { logNums } from './log-nums/index.js';
 
 console.log('============================');
 console.log('Celsius — Fahrenheit');
 console.log(100, 'C = ', toFahrenheit(100), 'F');
 console.log(212, 'F = ', toCelsius(212), 'C');
+console.log('============================');
+
+console.log('Log Numbers');
+logNums(10);
 console.log('============================');
---
+++ log-nums/index.ts
@@ -0,0 +1,7 @@
+const logNums = (n: number): void => {
+  for (let i = 1; i <= n; i++) {
+    console.log(i);
+  }
+};
+
+export { logNums };
