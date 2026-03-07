a07ec2c1468cdac604b1aa588522c954102ce892
Catgirl <zeroaninea@gmail.com>
2026-03-07 16:42:33 +0400

    the object methods called

--- data-structures/object.ts
+++ data-structures/object.ts
@@ -1,2 +1,29 @@
 console.log('============================');
 console.log('Object');
+
+const obj: {
+  name: string;
+  age: number;
+  'key-three': boolean;
+  hobby?: string;
+  sayMyName: () => void;
+} = {
+  name: 'Bruce',
+  age: 25,
+  'key-three': true,
+  sayMyName: () => console.log(obj.name),
+};
+obj.hobby = 'football';
+
+console.log(obj);
+console.log(obj.name);
+console.log(obj.age);
+console.log(obj['key-three']);
+console.log(obj.hobby);
+delete obj.hobby;
+console.log(obj);
+obj.sayMyName();
+
+Object.keys(obj).forEach((key) => console.log(key));
+Object.values(obj).forEach((value) => console.log(value));
+Object.entries(obj).forEach((entry) => console.log(entry));
