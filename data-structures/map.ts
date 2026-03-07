b73e7dcbf728ed0cdc08f26be28db34a1fe4b2e9
Catgirl <zeroaninea@gmail.com>
2026-03-07 16:54:20 +0400

    the methods of Map called

--- data-structures/map.ts
+++ data-structures/map.ts
@@ -4,8 +4,16 @@ console.log('Map');
 const map = new Map([
   ['a', 1],
   ['b', 2],
-  ['c', 3],
 ]);
+
+map.set('c', 3);
+console.log(map.has('c'));
+console.log(map.get('c'));
+console.log(map.size);
+map.delete('c');
+console.log(map.has('c'));
+console.log(map.size);
+map.clear();
 
 for (const [key, value] of map) {
   console.log(`${key}: ${value}`);
