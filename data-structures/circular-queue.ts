9a83f71fde7fa61ab21ee50fd4efc7b0292e1f97
Catgirl <zeroaninea@gmail.com>
2026-03-07 20:59:12 +0400

    circular queue implemented

--- data-structures/circular-queue.ts
+++ data-structures/circular-queue.ts
@@ -43,14 +43,39 @@ class CircularQueue<T> {
     this.currentLength--;
     return this.items[this.front];
   }
+
+  peek(): T | undefined {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+
+    return this.items[this.front];
+  }
+
+  print(): void {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+
+    let i;
+    let str = '';
+    for (i = 0; i < this.currentLength; i++) {
+      str += this.items[(this.front + i) % this.capacity] + ' ';
+    }
+    console.log(str);
+  }
 }
 
 const circularQueue = new CircularQueue<number>(3);
 circularQueue.enqcueue(1);
 circularQueue.enqcueue(2);
 circularQueue.enqcueue(3);
+console.log(circularQueue.peek());
+circularQueue.print();
 console.log(circularQueue.dequeue());
 console.log(circularQueue.dequeue());
 console.log(circularQueue.dequeue());
 console.log(circularQueue.isEmpty);
 console.log(circularQueue.isFull);
+circularQueue.print();
+console.log(circularQueue.peek());
