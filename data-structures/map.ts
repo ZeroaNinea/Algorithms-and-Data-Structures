e0988e271265e1ec590af0f62bedfd48c3755cb6
Catgirl <zeroaninea@gmail.com>
2026-03-07 16:52:44 +0400

    the map logged out

--- data-structures/map.ts
+++ data-structures/map.ts
@@ -1,2 +1,12 @@
 console.log('============================');
 console.log('Map');
+
+const map = new Map([
+  ['a', 1],
+  ['b', 2],
+  ['c', 3],
+]);
+
+for (const [key, value] of map) {
+  console.log(`${key}: ${value}`);
+}
